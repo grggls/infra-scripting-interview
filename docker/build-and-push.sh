01e8ea019971a3b80967bb4d7909302445c6a054
Gregory Damiani <gregory.damiani@mobimeo.com>
2023-05-03 18:50:42 +0200

    multi-stage build in the dockerfile, fix a bug in the build_and_push.sh script

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -5,14 +5,14 @@ ACCOUNT_ID="622140367382"
 REGION="us-west-2"
 REPO_PREFIX="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 SERVER_REPO_NAME="app"
-IMAGE_TAG=`git rev-parse HEAD`
+COMMIT_TAG=`git rev-parse HEAD`
 
 # Login to ECR
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${REPO_PREFIX}
 
-# Build and push the server image with a unique tag and 'latest'
-cd server
-docker build -t ${REPO_PREFIX}/${SERVER_REPO_NAME}:${IMAGE_TAG} .
-docker push ${REPO_PREFIX}/${SERVER_REPO_NAME}:${IMAGE_TAG}
-docker tag ${REPO_PREFIX}/${SERVER_REPO_NAME}:${IMAGE_TAG} ${REPO_PREFIX}/${SERVER_REPO_NAME}:latest
+# Build and push the server image with a unique tag for posterity
+docker build -t ${REPO_PREFIX}/${SERVER_REPO_NAME}:${COMMIT_TAG} .
+docker push ${REPO_PREFIX}/${SERVER_REPO_NAME}:${COMMIT_TAG}
+# Tag that same image with 'latest' and push. Tags are mutable in the ECR, so the 'latest' image will change with the next build_and_push
+docker tag ${REPO_PREFIX}/${SERVER_REPO_NAME}:${COMMIT_TAG} ${REPO_PREFIX}/${SERVER_REPO_NAME}:latest
 docker push ${REPO_PREFIX}/${SERVER_REPO_NAME}:latest
